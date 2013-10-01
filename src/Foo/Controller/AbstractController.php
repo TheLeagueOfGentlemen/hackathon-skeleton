8d3cf4b619a2d5b21c820c7f9d0be981a04c041a
Ben Glassman <ben@vtdesignworks.com>
2013-10-01 10:53:10 -0400

    Fix closing braces.

--- src/Foo/Controller/AbstractController.php
+++ src/Foo/Controller/AbstractController.php
@@ -30,7 +30,7 @@ abstract class AbstractController
             'type' => $type,
             'message' => $message
         ));
-       }
+    }
 
     protected function getSession()
     {
@@ -45,7 +45,7 @@ abstract class AbstractController
     protected function redirect($url)
     {
         return $this->app->redirect($url);
-       }
+    }
 
     protected function getValidator()
     {
@@ -55,17 +55,17 @@ abstract class AbstractController
     protected function generateUrl($route, $params = array())
     {
         return $this->app['url_generator']->generate($route, $params);
-       }
+    }
 
     protected function create404($message)
     {
         return $this->app->abort(404, $message);
-       }
+    }
 
     protected function create500($message)
     {
         return $this->app->abort(500, $message);
-       }
+    }
 
     protected function get($service)
     {
