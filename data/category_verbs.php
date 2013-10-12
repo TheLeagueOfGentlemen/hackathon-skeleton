1ea526b607e8f002d9572f71f9974192eb606221
Brian Cappello <briancappello@gmail.com>
2013-10-12 02:29:21 -0400

    broked stuffs

---
+++ data/category_verbs.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+    "Eat" => array("Eateries",
+                   ),
+    "Drink" => array("Breweries",
+                     "Wineries & Distilleries",
+                     ),
+    "Sleep" => array("Inns & B&B's",
+                     "Farm Stays",
+                     ),
+    "Learn" => array("Classes & Workshops",
+                     "Farm Visits",
+                     ),
+    "Shop" => array("Farmstands & U-Pick",
+                    "Farmers Markets",
+                    "Food Markets",
+                    "Cheese",
+                    "Maple Syrup",
+                    ),
+    "Hike" => array("Trails"),
+    "Ski (Alpine)" => array("Alpine Skiing"),
+    "Ski (Nordic)" => array("Nordic Skiing"),
+);
+
+/*
+// | Breweries               |
+// | Farmstands & U-Pick     |
+// | Inns & B&B's            |
+// | Farm Visits             |
+// | Eateries                |
+// | Classes & Workshops     |
+// | Wineries & Distilleries |
+// | Farmers Markets         |
+// | Cheese                  |
+// | Food Markets            |
+// | Maple Syrup             |
+// | Farm Stays              |
+// |                         |
+// | Trails                  |
+// | Nordic Skiing           |
+// | Alpine Skiing           |
+*/
