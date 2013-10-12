94b47eb1afcd0c8ca9f594f74dded72c9928d184
Max Eddy <maxiscool@gmail.com>
2013-10-12 02:03:04 -0400

    added adventure criteria

--- scripts/fixtures.sh
+++ scripts/fixtures.sh
@@ -1,3 +1,4 @@
 php scripts/populate_cities_counties.php
-php scripts/create_user_adventures.php
 bash scripts/populate_attractions.sh
+php scripts/create_user_adventures.php
+php scripts/create_adventure_criteria.php
