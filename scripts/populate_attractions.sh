574a8fcae22acd81b9602038913521d3cf6c9514
Brian Cappello <briancappello@gmail.com>
2013-10-12 03:04:52 -0400

    finish up category verbs

--- scripts/populate_attractions.sh
+++ scripts/populate_attractions.sh
@@ -3,3 +3,5 @@ php scripts/populate_digin_attractions.php
 php scripts/populate_trail_attractions.php
 php scripts/populate_ski_attractions.php
 php scripts/associate_attraction_cities.php
+
+php scripts/populate_category_verbs.php
